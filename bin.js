a13e6b2dd8c41c7edbf3661971491daad13f55c3
sameer-manek <sameermanek@hotmail.com>
2019-03-30 15:03:33 +0530

    completed fetching details about the word, todo, word of the day and word game

--- bin.js
+++ bin.js
@@ -2,13 +2,43 @@
 
 const program = require('commander')
 
-const { wordDefinition } = require("./index")
+const { 
+    wordDefinition, 
+    wordSynonymns,
+    wordAntonyms,
+    wordExamples,
+    wordAllDetails
+} = require("./index")
 
 program
-    .command('word <word>')
-    .alias('w <word>')
-    .description("fetch")
+    .command('lookup <word>')
+    .alias('l')
+    .description("fetch definitions for the given word")
     .action(word => wordDefinition(word))
+
+program
+    .command('synonyms <word>')
+    .alias('s')
+    .description("fetch synonyms of the given word")
+    .action(word => wordSynonymns(word))
+
+program
+    .command('antonyms <word>')
+    .alias('a')
+    .description("fetch antonyms of the given word")
+    .action(word => wordAntonyms(word))
+
+program
+    .command('examples <word>')
+    .alias('e')
+    .description("fetch examples of the given word")
+    .action(word => wordExamples(word))
+
+program
+    .command('details <word>')
+    .alias('d')
+    .description("fetch all the details about word")
+    .action(word => wordAllDetails(word))
     
 program.parse(process.argv)
 
--- index.js
+++ index.js
@@ -1,38 +1,101 @@
-const axios = require('axios')
+const Dictionary = require("oxford-dictionary");
+  
+const config = {
+    app_id : "7f2b66ee",
+    app_key : "9cabb81443299fff4bdd52d0242e5b8a",
+    source_lang : "en"
+};
 
-const api_id = "7f2b66ee"
-const api_key = "9cabb81443299fff4bdd52d0242e5b8a"
-
-const langauage = 'en'
-
-const base_uri = "https://od-api.oxforddictionaries.com/api/v1/"
-
-async function request(url) {
-    return await axios.get({
-        url,
-        headers: {
-            api_id,
-            api_key
-        }
-    }).then(({ result }) => (result)).catch(err => console.log(err))
-}
+const dict = new Dictionary(config)
 
 // word definition
 
 async function wordDefinition(word) {
-    console.log("this is called")
+    let lookup = dict.definitions(word.toLowerCase())
+
+    lookup.then(res => {
+        let results = res.results
+        console.log("following are the definitions:")
+        results.map(result => {
+            let definitions = result.lexicalEntries[0].entries[0].senses[0].definitions
+            definitions.map(definition => console.log(definition))
+        })
+    }, err => {
+        console.log(err)
+    })
 }
 
 // word synonyms
 
+async function wordSynonymns(word) {
+    let lookup = dict.synonyms(word.toLowerCase())
+
+    lookup.then(res => {
+        let results = res.results
+        console.log("following are the synonyms:")
+        results.map(result => {
+            let synonyms = result.lexicalEntries[0].entries[0].senses[0].synonyms
+            synonyms.map(synonym => console.log(synonym.text))
+        })
+    }, err => {
+        console.log(err)
+    })
+}
+
 // word antonyms
+
+async function wordAntonyms(word) {
+    let lookup = dict.antonyms(word.toLowerCase())
+
+    lookup.then(res => {
+        let results = res.results
+        console.log("following are the antonyms:")
+        results.map(result => {
+            let antonyms = result.lexicalEntries[0].entries[0].senses[0].antonyms
+            antonyms.map(antonym => console.log(antonym.text))
+        })
+    }, err => {
+        console.log(err)
+    })
+}
 
 // word examples
 
+async function wordExamples(word) {
+    let lookup = dict.sentences(word.toLowerCase())
+
+    lookup.then(res => {
+        let results = res.results
+        console.log("following are a few examples:")
+        results.map(result => {
+            let sentences = result.lexicalEntries[0].sentences
+            try {
+                for (let i = 0; i < 3; i += 1) {
+                    console.log(sentences[i].text)
+                }
+            } catch (e) {}
+        })
+    }, err => {
+        console.log(err)
+    })
+}
+
 // word all details, definitions, synonyms, antonyms, examples
+async function wordAllDetails(word) {
+    await wordDefinition(word)
+    await wordExamples(word)
+    await wordSynonymns(word)
+    await wordAntonyms(word)
+}
 
 // word of the day with all details
 
 // word game
 
-module.exports = { wordDefinition }
+module.exports = { 
+    wordDefinition,
+    wordSynonymns,
+    wordAntonyms,
+    wordExamples,
+    wordAllDetails
+}
